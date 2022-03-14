06e9aa10b702bf3ee05a2354baba6377754ef847
Space-Belt <dngurwn@gmail.com>
2022-03-14 20:37:02 +0900

    [Feat] #9 레포지토리 선택시 저장/삭제 기능

--- src/App.js
+++ src/App.js
@@ -21,8 +21,6 @@ export default App;
 const Wrapper = styled.div`
   width: 100%;
   height: 100vh;
-  /* max-width: 1920px; */
-  /* max-height: 1080px; */
   display: flex;
   flex-direction: column;
   justify-content: flex-start;
